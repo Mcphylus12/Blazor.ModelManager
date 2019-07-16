0dc88fe04c48ef0fb40ccd0584d2f79c72a827ad
Kyle Alexander <mcphylus@live.co.uk>
2019-07-16 17:20:37 +0200

    Added list Builder

---
+++ Site.Client/List/ListModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Site.Client.List
+{
+    public class ListModel
+    {
+        public IEnumerable<ListItem> Items { get; }
+    }
+
+    public class ListItem
+    {
+    }
+}
